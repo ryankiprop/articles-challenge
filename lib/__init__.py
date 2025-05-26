5285dca24ed429f0c69be7662b09176c7035f878
langatjnr <ryanlangatjnr@gmail.com>
2025-05-26 19:20:54 +0300

    init files completed

---
+++ lib/__init__.py
@@ -0,0 +1,4 @@
+from .connection import get_connection
+from .seed import seed_database
+
+__all__ = ['get_connection', 'seed_database']
---
+++ lib/db/__init__.py
@@ -0,0 +1,5 @@
+from lib.models.author import Author
+from lib.models.article import Article
+from lib.models.magazine import Magazine
+
+__all__ = ['Author', 'Article', 'Magazine']
