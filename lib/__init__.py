5ea696778899f2fd6a44616e9edce803ddef69d3
langatjnr <ryanlangatjnr@gmail.com>
2025-05-26 21:54:42 +0300

    reset due to errors

--- lib/__init__.py
+++
@@ -1,4 +0,0 @@
-from .connection import get_connection
-from .seed import seed_database
-
-__all__ = ['get_connection', 'seed_database']
--- lib/db/__init__.py
+++
@@ -1,5 +0,0 @@
-from lib.models.author import Author
-from lib.models.article import Article
-from lib.models.magazine import Magazine
-
-__all__ = ['Author', 'Article', 'Magazine']
